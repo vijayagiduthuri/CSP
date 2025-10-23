a90995953044336d1a9a809c39e838d1fc31ecbd
Vijaya Giduthuri <vijayagiduthuri2@gmail.com>
2025-10-23 23:43:32 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ if (!fs.existsSync(uploadsDir)) {
 
 // Middleware
 app.use(cors({
-  origin: ["https://csp-murex.vercel.app"], // add your frontend URL
+  origin: ["https://csp-kappa.vercel.app","http://localhost:5173"], // add your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
